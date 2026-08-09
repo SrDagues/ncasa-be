9315faa75f9528a79f2366c4e965b02dfafcddc6
Dagues <danicoin@gmail.com>
2026-08-09 11:32:41 +0200

    Initial Spring Boot project

---
+++ src/main/java/ncasa/NcasaApplication.java
@@ -0,0 +1,13 @@
+package ncasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NcasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NcasaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ncasa/NcasaApplicationTests.java
@@ -0,0 +1,13 @@
+package ncasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NcasaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
